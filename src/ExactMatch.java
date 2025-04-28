9981a06c119b706983b650e15f36e6fe53c8c7d9
wael <wael.hammali@etudiant-enit.utm.tn>
2025-04-28 14:08:54 +0100

    init commit

--- src/ExactMatch.java
+++ src/ExactMatch.java
@@ -1,7 +1,12 @@
 import java.util.Comparator;
 
-public class ExactMatch implements ComparateurDeDeuxNoms {
+public class ExactMatch implements ComparateurDeDeuxNoms,ComparateurDeDeuxStrings{
     public double comparer1(String s1, String s2) {
         if(s1.equals(s2)) return 1.0;
         return 0.0;
-}}
+}
+public double comparer2(String s1, String s2) {
+    if(s1.equals(s2)) return 1.0;
+    return 0.0;
+    }
+}
