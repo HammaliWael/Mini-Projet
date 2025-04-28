614c75b7cfa416d6b399ed98bbe862aa15e6b77c
wael <wael.hammali@etudiant-enit.utm.tn>
2025-04-28 12:08:08 +0100

    init commit

--- src/MyTuple.java
+++ src/MyTuple.java
@@ -2,11 +2,13 @@ public class MyTuple {
         String item1;
         String item2;
         double value;
+        double value2;
 
         public MyTuple(String item1, String item2, double value) {
             this.item1 = item1;
             this.item2 = item2;
             this.value = value;
+            this.value2 = value;
         }
     }
 
